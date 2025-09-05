72d5a29d4b981fccfe8c4d013a472d97ebdbb944
Markus Schmid <devildevsmith@protonmail.com>
2025-09-05 10:23:27 +0200

    Perf: Optimiere innere Schleife von Bubble Sort

--- MYFirstSolution/MYFirstSolution/BubbleSort.cs
+++ MYFirstSolution/MYFirstSolution/BubbleSort.cs
@@ -20,9 +20,9 @@ public class BubbleSort
         for (int i = 0; i < n - 1; i++)
         {
             // Die innere Schleife durchläuft das Array
-            for (int j = 0; j < n - 1; j++)
+            for (int j = 0; j < n - 1 - i; j++)
             {
-                if (arr[j] < arr[j + 1])
+                if (arr[j] > arr[j + 1])
                 {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
