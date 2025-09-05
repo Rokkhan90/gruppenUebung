b7285830cb32cf8db2657e041cf7cc11afe8c4d4
Markus Schmid <devildevsmith@protonmail.com>
2025-09-05 11:34:56 +0200

    Feat: Gib Array mit gelben Hntergrundd aus

--- MYFirstSolution/MYFirstSolution/BubbleSort.cs
+++ MYFirstSolution/MYFirstSolution/BubbleSort.cs
@@ -35,6 +35,9 @@ public class BubbleSort
 
     public static void DruckeArray(int[] arr)
     {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Black;
         Console.WriteLine(string.Join(", ", arr));
+        Console.ResetColor();
     }
 }
